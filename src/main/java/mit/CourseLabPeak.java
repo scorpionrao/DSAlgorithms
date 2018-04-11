dc918f8e0013ebc2881a7c008830b88ccbf01d06
Ranganatha Gonugunta <scorpionrao@yahoo.com>
2018-04-11 07:50:07 -0700

    Peak Finding Recursion

--- src/main/java/mit/CourseLabPeak.java
+++ src/main/java/mit/CourseLabPeak.java
@@ -3,7 +3,7 @@ package mit;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CourseLabPeak {
-	
+
 	public static void findPeakNaive(int[] array) {
 		long start = System.nanoTime();
 		boolean peakExists = false;
@@ -18,14 +18,62 @@ public class CourseLabPeak {
 			} else {
 				if(array[i] >= array[i-1] && array[i] >= array[i+1]) {
 					peakExists = true;
+					break;
 				}
 			}
 		}
 		System.out.println(peakExists);
 		System.out.println("Naive: " + (System.nanoTime() - start));
 	}
-	
-	public static void findPeakOptimized(int[] array) {
+
+	/*
+		psuedocode:
+		if(n<=1) {peak not found}
+		find middle
+		look at neighbors - O(1)
+		if greater than neighbors - peak found - O(1)
+		else recurse
+
+		T(n) = O(1) + T(n/2)
+		T(n) = O(1) + O(1) + T(n/4)
+		T(n) = O(log n)
+		T(1) = O(1)
+	 */
+	public static void findPeakOptimizedRecurse(int[] array) {
+		long start = System.nanoTime();
+
+		boolean peakExists = findPeakOptimizedRecurse(array, 0, array.length - 1);
+		System.out.println(peakExists);
+		System.out.println("Optimized Recursion: " + (System.nanoTime() - start));
+	}
+
+	/*
+	requires indexes in each recursion
+	do not require additional data structure to track anything
+	 */
+	private static boolean findPeakOptimizedRecurse(int[] array, int start, int end) {
+		// base case
+		if(start <= end) {
+			return false;
+		}
+
+		// Pre-process
+		int middle = start + (end - start) / 2;
+		boolean isTallerThanLeft = array[middle] > array[middle-1];
+		boolean isTallerThanRight = array[middle] > array[middle+1];
+
+		// Then decide - Stop, recurse left, recurse right
+		if(isTallerThanLeft && isTallerThanRight) {
+			return true;
+		} else if(!isTallerThanLeft){
+			return findPeakOptimizedRecurse(array, start, middle - 1);
+		} else {
+			return findPeakOptimizedRecurse(array, middle + 1, end);
+		}
+	}
+
+
+	public static void findPeakOptimizedIterative(int[] array) {
 		long start = System.nanoTime();
 		boolean peakExists = false;
 		int low = 0;
@@ -42,7 +90,7 @@ public class CourseLabPeak {
 			}
 		}
 		System.out.println(peakExists);
-		System.out.println("Optimized: " + (System.nanoTime() - start));
+		System.out.println("Optimized Iterative: " + (System.nanoTime() - start));
 	}
 	
 	public static void main(String[] args) {
@@ -53,7 +101,7 @@ public class CourseLabPeak {
 		}
 		System.out.println();
 		findPeakNaive(array);
-		findPeakOptimized(array);
+		findPeakOptimizedIterative(array);
 	}
 
 }
