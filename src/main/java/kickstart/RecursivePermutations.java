ccf17b48af2171b5c509da1af333058477c808fe
scorpionrao <scorpionrao@yahoo.com>
2018-04-17 13:28:30 -0700

    Update RecursivePermutations.java

--- src/main/java/kickstart/RecursivePermutations.java
+++ src/main/java/kickstart/RecursivePermutations.java
@@ -8,28 +8,43 @@ import java.util.Set;
 public class RecursivePermutations {
 
     public static void main(String[] args) {
-        wrapper("type");
+      wrapper("abc");
     }
 
     public static void wrapper(String str) {
-        // nP1, nP2 .... nPn
-        for(int i = 0; i < str.length(); i++) {
-            permutations("", str, i+1);
-        }
+      permutations(0,"", str, str.length());
     }
 
-    public static void permutations(String soFar, String remaining, int r) {
-        // nPn
-        // if(remaining.isEmpty())
-        // nPr
-        if(soFar.length() == r) {
+    /*
+    *   Prints all permutations upto size "R"
+    *
+    *   To print specific size ONLY, move print into end of recursion condition
+    *
+    *   Eg:
+    *                       ""
+    *       |               |               |
+    *       a(1)            b(6)            c(11)
+    *    |     |        |         |         |         |
+    *    ab(2) ac(4)    ba(7)     bc(9)     ca(12)    cb(14)
+    *    |     |        |         |         |         |
+    *   abc(3) acb(5)   bac(8)    bca(10)   cab(13)   cba(15)
+    */
+    public static void permutations(int level, String soFar, String remaining, int staticR) {
+        //System.out.println(String.format("Level: %d, SoFar: %s, Remaining: %s", inner, soFar, remaining));
+        // always print, saving 1 scenario, not a huge help
+        if(!soFar.isEmpty()) {
             System.out.println(soFar);
+        }
+
+        // remaining.isEmpty() -> soFar.length() == staticR
+        if(soFar.length() == staticR) {
+            // maximum required size of soFar reached, give it up
             return;
         }
         for(int i = 0; i < remaining.length(); i++) {
             String prefix = soFar + remaining.charAt(i);
             String suffix = remaining.substring(0, i) + remaining.substring(i+1);
-            permutations(prefix, suffix, r);
+            permutations(level+1, prefix, suffix, staticR);
         }
     }
 
@@ -47,6 +62,4 @@ public class RecursivePermutations {
         }
         return false;
     }
-
-
 }
