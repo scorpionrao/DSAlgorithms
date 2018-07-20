5b97a6d709de98b40ae03bf7a66b6eba2924c062
Ranganatha Gonugunta <scorpionrao@yahoo.com>
2018-07-20 13:26:26 -0700

    Longest Char Sequence

--- src/main/java/zr/WindowLongestUniqueSequence.java
+++ src/main/java/zr/WindowLongestUniqueSequence.java
@@ -8,31 +8,36 @@ public class WindowLongestUniqueSequence {
     public static class Solution {
 
         public String longestCharSequence(String str) {
-            if(str == null) {
-                return null;
+            if(str == null || str.isEmpty() || str.length() == 1) {
+                return str;
             }
-            int maxLength = Integer.MIN_VALUE;
-            String longestCharSequence = "";
+            int startIndex = 0;
+            int endIndex = 0;
+            int maxLength = endIndex - startIndex + 1;
             for(int startWindow = 0; startWindow < str.length(); startWindow++) {
                 for(int endWindow = startWindow; endWindow < str.length(); endWindow++) {
-                    // explain before hand.
-                    Set<Character> set = new HashSet<>();
-                    for(int i = startWindow; i <= endWindow; i++) {
-                        if(!set.contains(str.charAt(i))) {
-                            set.add(str.charAt(i));
-                        } else {
-                            // this window is disqualified
-                            break;
+                    if(maxLength >= (endWindow - startWindow + 1)) {
+                        continue;
+                    }
+                    Set<Character> charSet = new HashSet<>();
+                    for (int i = startWindow; i <= endWindow; i++) {
+                        charSet.add(str.charAt(i));
+                    }
+                    if(charSet.size() == (endWindow - startWindow + 1)) {
+                        // all characters are unique
+                        if(charSet.size() > maxLength) {
+                            // improves from previous best
+                            startIndex = startWindow;
+                            endIndex = endWindow;
+                            maxLength = endIndex - startIndex + 1;
                         }
-                        // all unique characters
-                        if(set.size() > maxLength) {
-                            maxLength = set.size();
-                            longestCharSequence = str.substring(startWindow, endWindow+1);
-                        }
+                    } else {
+                        // duplicate found. No need to increase the endWindow.
+                        break;
                     }
                 }
             }
-            return longestCharSequence;
+            return str.substring(startIndex, endIndex+1);
         }
     }
 
