539ef74f8b4b4bc58af4edd0914929e285e0a8f8
Ranganatha Gonugunta <scorpionrao@yahoo.com>
2018-09-11 12:30:06 -0700

    WindowNeedlePermutationsInHaystack

--- src/main/java/zr/WindowNeedlePermutationsInHaystack.java
+++ src/main/java/zr/WindowNeedlePermutationsInHaystack.java
@@ -1,50 +1,68 @@
 package zr;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class WindowNeedlePermutationsInHaystack {
 
-    public static Set<String> search(String S, String L) {
+    int numOfChars = 256;
 
-        int numOfChars = 256;
+    public Set<String> search(String S, String L) {
 
+        /* end cases */
+        if(S == null || S.isEmpty() || L == null || L.isEmpty()) {
+            return new HashSet<>();
+        }
+
+        /* DS 1 - index is char value */
         char[] freqS = new char[numOfChars];
         char[] freqW = new char[numOfChars];
 
+        /* O(S) */
         for(int i = 0; i < S.length(); i++) {
+            /* O(1) */
             freqS[S.charAt(i)]++;
             freqW[L.charAt(i)]++;
         }
 
         Set<String> result = new LinkedHashSet<>();
-
-        for(int endWindow = S.length() - 1; endWindow < L.length(); endWindow++) {
-            boolean success = true;
-            for(int j = 0; j < numOfChars; j++) {
-                if(freqS[j] != freqW[j]) {
-                    success = false;
-                    break;
-                }
-            }
-            if(success) {
+        /* O(L) */
+        for(int endWindow = S.length() - 1; endWindow < L.length() - 1; endWindow++) {
+            if(match(freqS, freqW)) {
                 String candidate = L.substring(endWindow - S.length() + 1, endWindow + 1);
                 result.add(candidate);
             }
 
-            freqW[L.charAt(endWindow - S.length() + 1)]--;
-            freqW[L.charAt(endWindow)]++;
+            int removeIndex = endWindow - S.length() + 1;
+            int addIndex = endWindow + 1;
+            freqW[L.charAt(removeIndex)]--;
+            freqW[L.charAt(addIndex)]++;
 
         }
-
+        /* O(numOfChars) */
+        if(match(freqS, freqW)) {
+            String candidate = L.substring(L.length() - S.length());
+            result.add(candidate);
+        }
         return result;
+    }
+
+    private boolean match(char[] freqS, char[] freqW) {
+        /* O(numOfChars) */
+        for(int j = 0; j < numOfChars; j++) {
+            if(freqS[j] != freqW[j]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
 
         String longString = "BACDGABCDA";
         String shortString = "ABCD";
-        Set<String> result = search(shortString, longString);
+        Set<String> result = new WindowNeedlePermutationsInHaystack().search(shortString, longString);
         System.out.println(result.toString());
     }
 }
