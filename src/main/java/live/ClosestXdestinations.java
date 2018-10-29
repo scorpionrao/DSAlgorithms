c986de0ed326784130302bd9874bc8de71de436c
Ranganatha Gonugunta <scorpionrao@yahoo.com>
2018-10-29 14:16:14 -0700

    Closest X Destinations

--- src/main/java/live/ClosestXdestinations.java
+++ src/main/java/live/ClosestXdestinations.java
@@ -1,84 +1,81 @@
 package live;
 
-import algorithms.cracking.KthToLastBST;
-
 import java.util.*;
 import java.lang.Math;
 
 public class ClosestXdestinations {
 
     public static class Node implements Comparable<Node> {
-        public int x;
-        public int y;
-        public double deliveryDistance;
+        int x;
+        int y;
+        double drivingDistance;
 
-        public Node(int x, int y) {
+        Node(int x, int y) {
             this.x = x;
             this.y = y;
-            calculateHypotenuse();
+            calculateDrivingDistance();
         }
 
-        private void calculateHypotenuse() {
-            int absX = Math.abs(x);
-            int absY = Math.abs(y);
-            this.deliveryDistance = Math.sqrt(Math.pow(absX, 2) + Math.pow(absY, 2));
+        public void calculateDrivingDistance() {
+            int absX = Math.abs(this.x);
+            int absY = Math.abs(this.y);
+            double absXSquare = Math.pow(absX, 2);
+            double absYSquare = Math.pow(absY, 2);
+            this.drivingDistance = Math.sqrt(absXSquare + absYSquare);
         }
 
         @Override
-        public int compareTo(Node o) {
-            return new Double(this.deliveryDistance).compareTo(new Double(o.deliveryDistance));
+        public int compareTo(Node other) {
+            if(this == other) {
+                return 0;
+            }
+            Double distance1 = new Double(this.drivingDistance);
+            Double distance2 = new Double(other.drivingDistance);
+            return distance1.compareTo(distance2);
         }
     }
 
-    public List<List<Integer>> ClosestXdestinations(int numDestinations,
-                                                    List<List<Integer>> allLocations,
-                                                    int numDeliveries) {
+    public List<List<Integer>> closestXDestinations(int numOfDestinations, List<List<Integer>> allLocations,
+                                                    int numOfDeliveries) {
 
-        if(allLocations == null || allLocations.isEmpty() || numDeliveries < 1) {
+        if(numOfDestinations < 1) {
             return new ArrayList<>();
         }
 /*
-        PriorityQueue<Node> pq = new PriorityQueue<>(allLocations.size(), new Comparator<Node>() {
-            @Override
-            public int compare(Node node1, Node node2) {
-                return new Double(node1.deliveryDistance).compareTo(new Double(node2.deliveryDistance));
-            }
-        });
+        PriorityQueue<Node> pq = new PriorityQueue<>(allLocations.size(),
+                new Comparator<Node>() {
+                    @Override
+                    public int compare(Node node1, Node node2) {
+                        Double distance1 = new Double(node1.drivingDistance);
+                        Double distance2 = new Double(node2.drivingDistance);
+                        return distance1.compareTo(distance2);
+                    }
+                });
 */
+
         PriorityQueue<Node> pq = new PriorityQueue<>(allLocations.size());
 
-
-        for(List<Integer> list : allLocations) {
-            pq.add(new Node(list.get(0), list.get(1)));
+        for(List<Integer> location : allLocations) {
+            pq.add(new Node(location.get(0), location.get(1)));
         }
 
         List<List<Integer>> result = new ArrayList<>();
-
-        while (!pq.isEmpty() && numDeliveries > 0) {
+        while(!pq.isEmpty() && numOfDeliveries > 0) {
             Node node = pq.poll();
-            List<Integer> coordinates = new ArrayList<>(Arrays.asList(node.x, node.y));
-            result.add(coordinates);
-            numDeliveries--;
+            List<Integer> nextLocation = new ArrayList<>(Arrays.asList(node.x, node.y));
+            result.add(nextLocation);
+            numOfDeliveries--;
         }
         return result;
     }
 
-    public static void main(String[] args)
-    {
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        list1.add(1);
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(2);
-        list2.add(2);
-        List<Integer> list3 = new ArrayList<>();
-        list3.add(2);
-        list3.add(2);
-        List<List<Integer>> allLocations = new ArrayList<>();
-        allLocations.add(list1);
-        allLocations.add(list2);
-        allLocations.add(list3);
-        List<List<Integer>> result = new ClosestXdestinations().ClosestXdestinations(3, allLocations, 2);
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 1));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(2, 2));
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<List<Integer>> allLocations = new ArrayList<>(Arrays.asList(list1, list2, list3));
+        List<List<Integer>> result =
+                new ClosestXdestinations().closestXDestinations(allLocations.size(), allLocations, 2);
         System.out.println(result.toString());
     }
 }
